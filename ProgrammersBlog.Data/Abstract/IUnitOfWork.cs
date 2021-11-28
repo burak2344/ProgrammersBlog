5887d50d0493bb949b607353ecaca24f76ec72a2
Burak Yunkul <byunkul23@hotmail.com>
2021-11-28 15:38:35 +0300

    UnitOfWork soyut ve somut sınıf ve interfaceleri oluşturuldu.

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+	public interface IUnitOfWork:IAsyncDisposable
+	{
+		//Burada Tüm Repositoryleri tek merkezden yöneteceğiz.
+		IArticleRepository Articles { get; }
+		ICommentRepository Comments { get; }
+		IRoleRepository Roles { get; }
+		IUserRepository Users { get; }
+		ICategoryRepository Categories { get; } //_unitOfWork.Categories.AddAsync();
+
+
+
+
+		//_unitOfWork.Categories.AddAsync(category);
+		//_unitOfWork.User.AddAsync(user);
+		//_unitOfWork.SaveAsync();
+		Task<int> SaveAsync();
+
+	}
+}
